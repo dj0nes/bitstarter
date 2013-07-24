c911caa7dd4614980db6c61add0192cc9ba980ee
Dillon Jones <dillon.z.jones@gmail.com>
2013-07-24 01:06:07 -0400

    Completed read from index.html

--- web.js
+++ web.js
@@ -2,9 +2,30 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+
+var fs = require('fs')
+  , filename = 'index.html';
+
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  console.log('OK: ' + filename);
+  response.send(data);
+
 });
+
+
+});
+
+
+/*
+app.get('/', function(request, response) {
+  response.send('Hello World 3!');
+});
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
