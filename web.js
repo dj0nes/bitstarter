f9aa0e78b239e5fb344a74cb4618e35eb65e03c3
Dillon Jones <dillon.z.jones@gmail.com>
2013-08-01 00:55:08 -0400

    fixed express warning

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
 var express = require('express');
+var http = require('http');
 var app = express();
 var fs = require('fs');
 var htmlfile = "index.html";
 
-var app = express.createServer(express.logger());
+app.set('port', process.env.PORT || 8080);
 
 app.get('/', function(request, response) {
     var html = fs.readFileSync(htmlfile).toString();
     response.send(html);
 });
 
-var port = process.env.PROT || 8080;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
+
+http.createServer(app).listen(app.get('port'), function(){ 
+            console.log('Server started at ' + app.get('port')); 
+      });
